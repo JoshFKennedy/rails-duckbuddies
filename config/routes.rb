bf3a29b49114e89368d1f1db2bc23678b4ace569
Josh Kennedy <joshkennedybjj@gmail.com>
2022-01-29 15:42:42 +0000

    added duck controller and cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :ducks, only: %i[index show new create] do
+  resources :ducks, only: %i[index show] do
     resources :booking, only: %i[index new create show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
