b1b695a14bdaf8933ff6c5cb42c56f90f461c0cc
James Britton <brittonjp@gmail.com>
2022-01-31 17:10:40 +0000

    corrected bookings routes and added link to new booking form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'ducks#index'
   resources :ducks, only: %i[index show] do
-    resources :booking, only: %i[index new create show]
+    resources :bookings, only: %i[index new create show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
